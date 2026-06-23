5bb057c8c7c6455c14caa5b62779885b788d959e
Rodrigo Diaz <dg.rodrigo.1503@gmail.com>
2026-06-24 00:47:58 +0200

    Fix: Conectando Django con el Frontend de React en Cloud Run

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,13 @@
-﻿from django.contrib import admin
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/auth/", include("apps.authentication.urls")),
-    path("api/tool/", include("apps.gradio_integration.urls")),
-    path("api/sheets/", include("apps.sheets.urls")),
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('apps.authentication.urls')),
+    path('api/sheets/', include('apps.sheets.urls')),
+    # CUALQUIER OTRA RUTA CARGA REACT
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
-
